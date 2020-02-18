549fcc45ff6cd7ae266b39eb2157391e73df45e4
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-18 17:57:05 +0700

    add exc

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -2,11 +2,11 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.restartCMD = async function (){
-    const { stdout, stderr } = await exec('clear');
+    const { stdout, stderr } = await exec('node stop server.js');
     console.log('restart 1');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
-    const { stdout2, stderr2 } = await exec('npm start');
+    const { stdout2, stderr2 } = await exec('node server.js');
     console.log('stdout2:', stdout2);
     console.log('stderr2:', stderr2);
   };
