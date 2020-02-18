fda9d4e2c11b8a4bc835509231a296e8eb7cb6e4
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-18 17:48:51 +0700

    two exec

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -1,10 +1,10 @@
-const { exec } = require("child_process");
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-exports.restartCMD = function (){
+exports.restartCMD = async function (){
+    const { stdout, stderr } = await exec('^C');
+    console.log('restart 1');
     const { stdout, stderr } = await exec('npm start');
-    console.log('restart 2');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
   };
