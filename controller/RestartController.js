72c54f90733871c173071eea717cca319b80c655
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-18 17:56:14 +0700

    add clear

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -2,9 +2,11 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.restartCMD = async function (){
-    const { stdout, stderr } = await exec('^C');
+    const { stdout, stderr } = await exec('clear');
     console.log('restart 1');
-    const { stdout, stderr } = await exec('npm start');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
+    const { stdout2, stderr2 } = await exec('npm start');
+    console.log('stdout2:', stdout2);
+    console.log('stderr2:', stderr2);
   };
