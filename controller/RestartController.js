9e7837268b904dfd05adfc39d7398f76f35882be
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-18 17:45:26 +0700

    add hook 3

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -2,13 +2,9 @@ const { exec } = require("child_process");
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-async function ls() {
-}
-
-
 exports.restartCMD = function (){
     const { stdout, stderr } = await exec('npm start');
-    console.log('restart');
+    console.log('restart 2');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
   };
