18bba9236e88f8cf4b558b9e9d22023226fb942d
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-19 09:27:41 +0700

    something

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -3,11 +3,11 @@ const exec = util.promisify(require('child_process').exec);
 
 exports.restartCMD = async function (){
   console.log("something working");
-    // const { stdout, stderr } = await exec('killall node');
-    // console.log('restart 1');
-    // console.log('stdout:', stdout);
-    // console.log('stderr:', stderr);
-    const { stdout2, stderr2 } = await exec('git pull');
+    const { stdout, stderr } = await exec('git pull');
+    console.log('restart 1');
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+    const { stdout2, stderr2 } = await exec('npm start');
     console.log('stdout2:', stdout2);
     console.log('stderr2:', stderr2);
   };
