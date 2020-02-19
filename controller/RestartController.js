9d93205251f9a8a7bb15567d281894960cedd4cf
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-19 09:50:42 +0700

    add nodemon

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -3,10 +3,10 @@ const exec = util.promisify(require('child_process').exec);
 
 exports.restartCMD = async function (){
   console.log("something working");
-    const { stdout, stderr } = await exec('git pull');
-    console.log('restart 1');
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
+    // const { stdout, stderr } = await exec('git pull');
+    // console.log('restart 1');
+    // console.log('stdout:', stdout);
+    // console.log('stderr:', stderr);
     const { stdout2, stderr2 } = await exec('npm start');
     console.log('npm start again');
     console.log('stdout2:', stdout2);
