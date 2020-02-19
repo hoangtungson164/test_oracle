4d2e9e78d1a8cb1fdccbb09cdce6a937834f3f68
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-19 09:20:27 +0700

    adding something

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -2,11 +2,11 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.restartCMD = async function (){
-    const { stdout, stderr } = await exec('taskkill /F /IM');
-    console.log('restart 1');
-    console.log('stdout:', stdout);
-    console.log('stderr:', stderr);
-    const { stdout2, stderr2 } = await exec('node server.js');
+    // const { stdout, stderr } = await exec('killall node');
+    // console.log('restart 1');
+    // console.log('stdout:', stdout);
+    // console.log('stderr:', stderr);
+    const { stdout2, stderr2 } = await exec('git pull');
     console.log('stdout2:', stdout2);
     console.log('stderr2:', stderr2);
   };
