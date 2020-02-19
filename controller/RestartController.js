699630f5413943bfd4629f176d7d7903c8fdc16a
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-19 09:04:07 +0700

    add something

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -2,7 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 exports.restartCMD = async function (){
-    const { stdout, stderr } = await exec('ps aux | grep node');
+    const { stdout, stderr } = await exec('taskkill /F /IM');
     console.log('restart 1');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
