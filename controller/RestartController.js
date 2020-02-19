9be007132ec65e4973895afe31b34dd31fd14ab6
Hoang Tung Son <hoangtungson164@gmail.com>
2020-02-19 09:51:53 +0700

    line

--- controller/RestartController.js
+++ controller/RestartController.js
@@ -7,8 +7,4 @@ exports.restartCMD = async function (){
     console.log('restart 1');
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
-    const { stdout2, stderr2 } = await exec('npm start');
-    console.log('npm start again');
-    console.log('stdout2:', stdout2);
-    console.log('stderr2:', stderr2);
   };
